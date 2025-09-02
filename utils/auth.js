b2b7e372b38dab23d5f086fc95085b95247ddee6
Mariia D <mariia.demidova0304@gmail.com>
2025-09-02 10:26:54 -0400

    Fixed a little bug in auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -5,7 +5,7 @@ const expiration = '2h';
 
 const authMiddleware = (req, res, next) => {
     // Allows token to be sent via req.body, req.query, or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+    let token = req.body?.token || req.query.token || req.headers.authorization;
 
     // We split the token string into an array and return actual token
     if (req.headers.authorization) {
